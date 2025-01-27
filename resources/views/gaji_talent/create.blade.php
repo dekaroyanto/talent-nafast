a7282300a7bf259b1861844fb263990556018b0e
dekaroyanto <dekaroy05@gmail.com>
2025-01-27 12:26:54 +0700

    [ADD] Gaji Talent

--- resources/views/gaji_talent/create.blade.php
+++ resources/views/gaji_talent/create.blade.php
@@ -3,76 +3,85 @@
 @section('content')
     <div class="container">
         <h2>Create Gaji Talent</h2>
-        <form action="{{ route('gaji-talent.store') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="talent_id">Talent</label>
-                <select name="talent_id" id="talent_id" class="form-control">
-                    <option value="">Select Talent</option>
-                    @foreach ($talents as $talent)
-                        <option value="{{ $talent->id }}">{{ $talent->nama_talent }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="periode_gaji_awal">Periode Gaji Awal</label>
-                <input type="date" class="form-control" name="periode_gaji_awal" id="periode_gaji_awal" required>
-            </div>
-            <div class="form-group">
-                <label for="periode_gaji_akhir">Periode Gaji Akhir</label>
-                <input type="date" class="form-control" name="periode_gaji_akhir" id="periode_gaji_akhir" required>
-            </div>
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('gaji-talent.store') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="talent_id">Talent</label>
+                        <select name="talent_id" id="talent_id" class="form-control">
+                            <option value="">Select Talent</option>
+                            @foreach ($talents as $talent)
+                                <option value="{{ $talent->id }}">{{ $talent->nama_talent }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="periode_gaji_awal">Periode Gaji Awal</label>
+                        <input type="date" class="form-control" name="periode_gaji_awal" id="periode_gaji_awal" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="periode_gaji_akhir">Periode Gaji Akhir</label>
+                        <input type="date" class="form-control" name="periode_gaji_akhir" id="periode_gaji_akhir"
+                            required>
+                    </div>
 
-            <h4>Perhitungan Gaji Talent</h4>
-            <table class="table">
-                <tr>
-                    <td>Fee Live per Jam</td>
-                    <td><input type="number" name="fee_live_perjam" id="fee_live_perjam" class="form-control" readonly>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Fee Take Video per Jam</td>
-                    <td><input type="number" name="fee_take_video_perjam" id="fee_take_video_perjam" class="form-control"
-                            readonly></td>
-                </tr>
-                <tr>
-                    <td>Total Lama Sesi Live</td>
-                    <td><input type="number" name="total_lama_sesi_live" id="total_lama_sesi_live" class="form-control"
-                            readonly></td>
-                </tr>
-                <tr>
-                    <td>Total Lama Sesi Take Video</td>
-                    <td><input type="number" name="total_lama_sesi_take_video" id="total_lama_sesi_take_video"
-                            class="form-control" readonly></td>
-                </tr>
-                <tr>
-                    <td>Fee Live Didapat</td>
-                    <td><input type="number" name="fee_live_didapat" id="fee_live_didapat" class="form-control" readonly>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Fee Take Video Didapat</td>
-                    <td><input type="number" name="fee_take_video_didapat" id="fee_take_video_didapat" class="form-control"
-                            readonly></td>
-                </tr>
-                <tr>
-                    <td>Jumlah Total Omset</td>
-                    <td><input type="number" name="jumlah_total_omset" id="jumlah_total_omset" class="form-control"
-                            readonly></td>
-                </tr>
-                <tr>
-                    <td>Rate Omset per Jam</td>
-                    <td><input type="number" name="rate_omset_perjam" id="rate_omset_perjam" class="form-control" readonly>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Total Gaji</td>
-                    <td><input type="number" name="total_gaji" id="total_gaji" class="form-control" readonly></td>
-                </tr>
-            </table>
+                    <h4>Perhitungan Gaji Talent</h4>
+                    <div class="form-floating mb-2">
+                        <input type="number" name="fee_live_perjam" id="fee_live_perjam" class="form-control" readonly>
+                        <label for="fee_live_perjam">Fee Live per Jam</label>
+                    </div>
 
-            <button type="submit" class="btn btn-primary">Save</button>
-        </form>
+                    <div class="form-floating mb-2">
+                        <input type="number" name="fee_take_video_perjam" id="fee_take_video_perjam" class="form-control"
+                            readonly>
+                        <label for="fee_take_video_perjam">Fee Take Video per Jam</label>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <input type="number" name="total_lama_sesi_live" id="total_lama_sesi_live" class="form-control"
+                            readonly>
+                        <label for="total_lama_sesi_live">Total Lama Sesi Live</label>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <input type="number" name="total_lama_sesi_take_video" id="total_lama_sesi_take_video"
+                            class="form-control" readonly>
+                        <label for="total_lama_sesi_take_video">Total Lama Sesi Take Video</label>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <input type="number" name="fee_live_didapat" id="fee_live_didapat" class="form-control" readonly>
+                        <label for="fee_live_didapat">Fee Live Didapat</label>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <input type="number" name="fee_take_video_didapat" id="fee_take_video_didapat" class="form-control"
+                            readonly>
+                        <label for="fee_take_video_didapat">Fee Take Video Didapat</label>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <input type="number" name="jumlah_total_omset" id="jumlah_total_omset" class="form-control"
+                            readonly>
+                        <label for="jumlah_total_omset">Jumlah Total Omset</label>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <input type="number" name="rate_omset_perjam" id="rate_omset_perjam" class="form-control" readonly>
+                        <label for="rate_omset_perjam">Rate Omset per Jam</label>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <input type="number" name="total_gaji" id="total_gaji" class="form-control" readonly>
+                        <label for="total_gaji">Total Gaji</label>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
+            </div>
+        </div>
+
     </div>
 
     <script>
